0870257776e1680f51b38b2574a388844376e04b
Josh Linton <lintonjh@gmail.com>
2018-07-14 14:51:52 -0700

    integrated jwt with passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 // Load user model
 const User = require("../../models/User");
@@ -89,5 +90,20 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+//  @route  GET api/users/current
+//  @desc   Return current user
+//  @access Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
