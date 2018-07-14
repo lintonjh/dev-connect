268ab919961e26cc8c047084f72f5f212c590171
demo <demo@demos-MacBook-Pro.local>
2018-07-14 12:15:12 -0700

    set and adding of mongoDB

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//DB Config
+const db = process.env.MONGOURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
